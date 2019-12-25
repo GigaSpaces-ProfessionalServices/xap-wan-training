e4daba1f318f6fc8c46b238d598531b23a3696af
yuval <yuvald@gigaspaces.com>
2019-12-25 14:19:47 +0200

    refactor: change 123 the gigaspaces

---
+++ BillBuddyModel/src/main/java/com/gigaspaces/training/billbuddy/model/AccountStatus.java
@@ -0,0 +1,12 @@
+package com.gigaspaces.training.billbuddy.model;
+
+/** 
+* AccountStatus class is an Enum which indicate merchant and user account status 
+* 
+* @author Gigaspaces
+*/
+public enum AccountStatus {
+	ACTIVE,
+    BLOCKED,
+    INACTIVE;
+}
