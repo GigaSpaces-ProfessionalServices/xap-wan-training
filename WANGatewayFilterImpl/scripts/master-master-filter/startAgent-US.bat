274a2d1c10ba277a5f58a147febe6d07cf9d63b9
yuval <yuvald@gigaspaces.com>
2019-12-28 13:06:45 +0200

    rename system variables

--- WANGatewayFilterImpl/scripts/master-master-filter/startAgent-US.bat
+++ WANGatewayFilterImpl/scripts/master-master-filter/startAgent-US.bat
@@ -5,10 +5,10 @@ set GS_LOOKUP_GROUPS="US"
 rem PLEASE replace 127.0.0.1 with relevant HOSTNAME in production
 set GS_LOOKUP_LOCATORS=127.0.0.1:4266
 
-set EXT_JAVA_OPTIONS=%EXT_JAVA_OPTIONS% -Dcom.sun.jini.reggie.initialUnicastDiscoveryPort=4266
-set EXT_JAVA_OPTIONS=%EXT_JAVA_OPTIONS% -Dcom.gigaspaces.system.registryPort=10198
-set EXT_JAVA_OPTIONS=%EXT_JAVA_OPTIONS% -Dcom.gigaspaces.start.httpPort=9913
-set EXT_JAVA_OPTIONS=%EXT_JAVA_OPTIONS% -Dcom.gs.zones=US
+set GS_OPTIONS_EXT=%GS_OPTIONS_EXT% -Dcom.sun.jini.reggie.initialUnicastDiscoveryPort=4266
+set GS_OPTIONS_EXT=%GS_OPTIONS_EXT% -Dcom.gigaspaces.system.registryPort=10198
+set GS_OPTIONS_EXT=%GS_OPTIONS_EXT% -Dcom.gigaspaces.start.httpPort=9913
+set GS_OPTIONS_EXT=%GS_OPTIONS_EXT% -Dcom.gs.zones=US
 
 rem Modify this as needed
 set GS_GSC_OPTIONS=-Xmx128m
