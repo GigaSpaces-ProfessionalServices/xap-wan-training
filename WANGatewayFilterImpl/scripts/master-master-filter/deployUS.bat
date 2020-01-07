e6970693ebc2e32226b7b6035a03ff450752a8b2
yuval <yuvald@gigaspaces.com>
2020-01-07 13:24:23 +0200

    added full path for gs.bat

--- WANGatewayFilterImpl/scripts/master-master-filter/deployUS.bat
+++ WANGatewayFilterImpl/scripts/master-master-filter/deployUS.bat
@@ -4,8 +4,7 @@ xcopy /E/I/F  %~dp0..\..\deploy\wan-gateway-US %GS_HOME%\deploy\wan-gateway-US
 xcopy /E/I/F  %~dp0..\..\deploy\wan-space-US %GS_HOME%\deploy\wan-space-US
 xcopy /E/I/F ..\..\..\ReplicationFilter\target\classes\com ..\..\deploy\wan-space-US
 
-cd %GS_HOME%\bin
 rem PLEASE replace 127.0.0.1 with relevant HOSTNAME in production
 set GS_LOOKUP_LOCATORS=127.0.0.1:4266
-call gs.bat --cli-version=1 deploy -zones US wan-space-US
-call gs.bat --cli-version=1 deploy -zones US wan-gateway-US
+call %GS_HOME%\bin\gs.bat --cli-version=1 deploy -zones US wan-space-US
+call %GS_HOME%\bin\gs.bat --cli-version=1 deploy -zones US wan-gateway-US
